ac32785def12e1cee17cb7a8e69afbf177cf561b
Harsh Kumar <harshkumar.contact@gmail.com>
2026-07-10 12:34:15 +0000

    data types

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,18 @@
+"use strict" // treat all js code as new version
+// alert(3+3) // we are using nodejs not browser
+
+let name = "harsh"
+let age = 18
+let isHandsome = false
+let state;
+
+// bigint for bigger numbers
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+//object
+
+console.log(typeof null);
