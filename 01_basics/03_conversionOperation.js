08864a5dedfc4e50071f197620ea165feada8f77
Harsh Kumar <harshkumar.contact@gmail.com>
2026-07-10 13:45:09 +0000

    conversion and operations

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,49 @@
+let score = "harsh"
+
+// console.log(typeof score); // can write both ways for finding type
+// console.log(typeof(score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true = 1; false = 0;
+
+let isLoggedIn = "harsh"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(typeof booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn)
+// 1 => true; 0 => false
+// "" => false
+// "harsh" => true
+
+let someNumber = 33;
+let stringNumber = String(someNumber)
+// console.log(typeof stringNumber);
+// console.log(stringNumber);
+
+/*******Operations ******/
+
+let num = 3
+let negnum = -num
+// console.log(negnum);
+
+// console.log(2+2);
+// console.log(2*2);
+// console.log(2**3); for power
+// console.log(2/2);
+// console.log(2%2); for remainder  
+
+// console.log("2"+1);
+// console.log(2+3+"1" );
+// console.log("2"+1+5 );
+// console.log("2"+"1" );
+
+console.log(+true);
+console.log(+"");
+
+let gameCounter = 100
+gameCounter++
+console.log(gameCounter);
