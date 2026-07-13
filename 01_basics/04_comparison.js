1183a21dc2c57052125e91a793b92869d5c96c8e
Harsh Kumar <harshkumar.contact@gmail.com>
2026-07-13 04:48:28 +0000

    Comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,17 @@
+// console.log(2>3);
+// console.log( 2>=3 );
+// console.log( 2<=3 );
+// console.log( 2<3 );
+// console.log( 2==3 );
+// console.log( 2!=3 );
+
+// console.log( "2">1 );
+// console.log( "02">1 );
+
+// datatype should be same else its output can differ time to time
+console.log( null == 0 );
+console.log( null > 0 );
+console.log( null >= 0 );
+
+// === for stict check
+console.log("2" === 2);
